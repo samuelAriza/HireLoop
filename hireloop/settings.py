f443ecd8a8b6c001bbaf802aaf78c9359b4a8125
samuelAriza <samargo0317@gmail.com>
2025-11-01 15:40:06 -0500

    chore(settings.py)

--- hireloop/settings.py
+++ hireloop/settings.py
@@ -152,11 +152,14 @@ WSGI_APPLICATION = "hireloop.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", "hireloop"),
+        "USER": os.getenv("DB_USER", "webuser"),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DATABASE_HOST", "127.0.0.1"),  # proxy local dentro del pod
+        "PORT": os.getenv("DATABASE_PORT", "5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
