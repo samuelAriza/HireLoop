e568c4721234acb437e431468d3026377e8eef41
samuelAriza <samargo0317@gmail.com>
2025-11-01 13:14:58 -0500

    chore(settings.py)

--- hireloop/urls.py
+++ hireloop/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for hireloop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -24,8 +7,6 @@ from django.http import JsonResponse
 
 def root_health_check(request):
     return JsonResponse({"status": "ok"}, status=200)
-
-
 
 
 urlpatterns = [
@@ -42,6 +23,8 @@ urlpatterns = [
     path("analytics/", include("analytics.urls", namespace="analytics")),
     path("django_plotly_dash/", include("django_plotly_dash.urls")),
     path("api/", include("microservices.api.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+
     path("", root_health_check, name="root_health_check"),
 ]
 
