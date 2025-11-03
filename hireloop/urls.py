a2b349aef49cd9529918c181e688c7cabe12e8d3
samuelAriza <samargo0317@gmail.com>
2025-11-02 22:30:57 -0500

    chore(urls)

--- hireloop/urls.py
+++ hireloop/urls.py
@@ -55,6 +55,4 @@ urlpatterns = [
     path("ready/", readiness_check, name="root_readiness_check"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
